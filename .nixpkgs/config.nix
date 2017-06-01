92b771b69490e93b90849dafa7b9b6b4a10326ae
Nicholas Clarke <nick@topos.org.uk>
2017-06-01 17:57:44 +0100

    Add relevant packages to .nixpkgs/config

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -1,26 +1,40 @@
 # ~/.nixpkgs/config.nix
 
 {
+  allowUnfree = true;
+  
   packageOverrides = pkgs_: with pkgs_; {  # pkgs_ is the original set of packages
     all = with pkgs; buildEnv {  # pkgs is your overriden set of packages itself
       name = "all";
       paths = [
+        # Web stuff
+        chromium
+        slack
+        tor-browser-bundle-bin
+
+        # Develpoment tools
         dos2unix
-        ghc
         git
-        graphviz
+        gnumake
+        sublime3
+        zeal
+
+        # Haskell Packages
+        ghc
         haskellPackages.hindent
         haskellPackages.hlint
-#       haskellPackages.hsdev
+#        haskellPackages.hsdev
         haskellPackages.stylish-haskell
+        stack
+
+        # Rust
+        rustup
+
+        # System
         htop
         mosh
         nox
         p7zip
-#       pijul
-        rustup
-        stack
-        stoken
         tmate
         tmux
       ];
