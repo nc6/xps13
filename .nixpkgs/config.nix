797141662cf57f3e9c3ea8a4c5d460a8af5bf3aa
Nicholas Clarke <nick@topos.org.uk>
2017-12-20 11:39:29 +0000

    More updates

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -8,8 +8,21 @@
     haskellPackages = self.haskellPackages.override {
       overrides = hsSelf: hsSuper: {
         numhask = hsSelf.callPackage /home/nc/proj/numhask {};
+        stackage2nix = hsSelf.callPackage /home/nc/proj/stackage2nix/nix/stackage2nix {};
       };
     };
+
+    remacs = with pkgs; self.callPackage /home/nc/proj/remacs.nixpkg {
+        libXaw = xorg.libXaw;
+        Xaw3d = null;
+        gconf = null;
+        alsaLib = null;
+        imagemagick = null;
+        acl = null;
+        gpm = null;
+        rustPlatform = recurseIntoAttrs (makeRustPlatform pkgs.rustChannels.nightly);
+        inherit (darwin.apple_sdk.frameworks) AppKit CoreWLAN GSS Kerberos ImageIO;
+      };
 
     all = with self; buildEnv {
       name = "all";
@@ -23,11 +36,14 @@
         slack
 
         # Develpoment tools
+        ag
         binutils
         gcc
         git
         gnumake
         gitAndTools.hub
+#       remacs
+        source-code-pro
         sublime3-dev
         zeal
 
@@ -36,29 +52,40 @@
 
         # Haskell Packages
         ghc
+        haskellPackages.hasktags
         haskellPackages.hindent
         haskellPackages.hlint
-#        haskellPackages.hsdev
+        haskellPackages.stackage2nix
         haskellPackages.stylish-haskell
         stack
+
+        # Idris
+        idris
+
+        # Latex
+        (texlive.combine {
+          inherit (texlive) scheme-small listings glossaries mfirstuc parskip xfor;
+        })
+        texmaker
 
         # Nix tools
         nix-repl
 
         # Purescript
-#       purescript
-        psc-package
+        #purescript
+        #psc-package
 
         # R
         R
 
         # Rust
-        pkgs.rustChannels.beta.rust
+        pkgs.rustChannels.nightly.rust
 
         # System
         htop
         gnupg
         gwenview
+        kdeconnect
         kgpg
         mosh
         p7zip
